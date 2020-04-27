7fa1ec0f06960967280a0734e24ead19be54a919
Ellis Trickett <ellis.trickett@gmail.com>
2020-04-27 16:08:53 +0100

    added ERB file

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  "Hello Maker"
+  erb(:index)
 end
 
 get '/secret' do
@@ -9,7 +9,7 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<div>
+  "<div style='border: 3px dashed red'>
   <img src='http://bit.ly/1eze8aE'>
   </div>"
 end
